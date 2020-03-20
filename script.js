dca806c9a15581d8f3dc8170ec71a83c069662b2
Lou Stringer <louise.stringer@gmail.com>
2020-03-20 00:57:59 +0000

    add basic css

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-//let businessList = null
-
 const renderBusinesses = (data, tabletop) => {
-  document.querySelector(".businesses").innerText = "";
+  document.querySelector(".waiting").classList.toggle("hide");
   for (let i=0; i<data.length; i++) {
-    console.log(data[i].name)
     makeBusinessItem(data[i]);
   }
 }
+
 const init = () => {
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/17h_0IxZl0K2neRsKnBPXTWh1nHdh8CuFXTEf2kqEE48/edit?usp=sharing',
                    callback: renderBusinesses,
@@ -15,23 +13,28 @@ const init = () => {
 window.addEventListener('DOMContentLoaded', init)
 
 const makeBusinessItem = (businessData) => {
-  let html = '<li class="name"><p>%name%</p><p><span class="businessInfo">booze</span><span class="businessInfo">food</span></p><p><span class="businessInfo">takeaway</span><span class="businessInfo">delivery to %deliveryPlace%</p><p>%notes%</p><p>twitter %twitterName%</p><p><a href="%link%">Check out %name%\'s website</a></p></li>'
+  let html = '<div class="business allBorder"><h3 class="name">%name%</h3><p><span class="businessInfo booze">booze</span><span class="businessInfo food">food</span></p><p><span class="businessInfo takeaway">takeaway</span><span class="businessInfo delivery">delivery</p><p>Pick up/delivery: %deliveryPlace%</p><p>%notes%</p><p>twitter <a href="https://twitter.com/%twitterLink%">%twitterName%</p><p><a href="%link%">%name%\'s website</a></p></div>'
   html = html.replace("%name%", businessData.name);
   if (businessData.booze == "FALSE") {
-    html = html.replace("booze", "");
+    html = html.replace("businessInfo booze", "businessInfo booze nope");
   };
   if (businessData.food == "FALSE") {
-    html = html.replace("food", "");
+    html = html.replace("businessInfo food", "businessInfo food nope");
   };
   if (businessData.takeaway == "FALSE") {
-    html = html.replace("takeaway", "");
+    html = html.replace("businessInfo takeaway", "businessInfo takeaway nope");
   };
-  if (businessData.delivery == "TRUE") {
-    html = html.replace("%deliveryPlace%", businessData.deliversTo);
-  } else {
-    html = html.replace("delivery to %deliveryPlace%", "");
+  if (businessData.delivery == "FALSE") {
+    html = html.replace("businessInfo delivery", "businessInfo delivery nope");
+  }
+  html = html.replace("%deliveryPlace%", businessData.deliversTo);
+  if (businessData.deliversTo == "York") {
+    html = html.replace("class=\"business allBorder\"", "class=\"business yBorder\"")
+  } else if (businessData.deliversTo == "Harrogate & Knaresborough") {
+    html = html.replace("class=\"business allBorder\"", "class=\"business hkBorder\"")
   };
   html = html.replace("%notes%", businessData.notes);
+  html = html.replace("%twitterLink%", businessData.twitter.substring(1));
   html = html.replace("%twitterName%", businessData.twitter);
   html = html.replace("%link%", businessData.link);
   html = html.replace("%name%", businessData.name);
