8893bb5f15710db08949766459ac2ad9e40d1a08
Lou Stringer <louise.stringer@gmail.com>
2020-03-24 22:22:28 +0000

    lockdown update

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const renderBusinesses = (data, tabletop) => {
-  //document.querySelector(".waiting").classList.remove(".hide");
+let businesses = document.querySelector(".businesses")
+
+const renderBusinessesYork = (data, tabletop) => {
   for (let i=0; i<data.length; i++) {
-    makeBusinessItem(data[i]);
+    if (data[i].active == "TRUE" && data[i].york == "TRUE") {
+      makeBusinessItem(data[i]);
+      businesses.lastChild.classList.add("yBorder");
+    }
   }
 }
 
-const init = () => {
-  //document.querySelector(".businesses").innerText = "";
-  //document.querySelector(".waiting").classList.add(".hide");
+const renderBusinessesHk = (data, tabletop) => {
+  for (let i=0; i<data.length; i++) {
+    if (data[i].active == "TRUE" && data[i].hk == "TRUE") {
+      makeBusinessItem(data[i]);
+      businesses.lastChild.classList.add("hkBorder");
+    }
+  }
+}
+
+const initYork = () => {
+  businesses.innerHTML = "";
   Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/17h_0IxZl0K2neRsKnBPXTWh1nHdh8CuFXTEf2kqEE48/edit?usp=sharing',
-                   callback: renderBusinesses,
+                   callback: renderBusinessesYork,
+                   simpleSheet: true } )
+}
+
+const initHk = () => {
+  businesses.innerHTML = "";
+  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/17h_0IxZl0K2neRsKnBPXTWh1nHdh8CuFXTEf2kqEE48/edit?usp=sharing',
+                   callback: renderBusinessesHk,
                    simpleSheet: true } )
 }
 
 const makeBusinessItem = (businessData) => {
-  let html = '<div class="business allBorder"><h3 class="name">%name%</h3><p><span class="businessInfo booze">booze</span><span class="businessInfo food">food</span></p><p><span class="businessInfo takeaway">takeaway</span><span class="businessInfo delivery">delivery</p><p>Pick up/delivery: %deliveryPlace%</p><p>%notes%</p><p>twitter <a href="https://twitter.com/%twitterLink%">%twitterName%</p><p><a href="%link%">%name%\'s website</a></p></div>'
+  let html = '<div class="business"><h3 class="name">%name%</h3><p><span class="businessInfo booze">booze</span><span class="businessInfo food">food</span></p><p><span class="businessInfo takeaway">takeaway</span><span class="businessInfo delivery">delivery</p><p>%notes%</p><p>twitter <a href="https://twitter.com/%twitterLink%">%twitterName%</p><p><a href="%link%">%name%\'s website</a></p></div>'
   html = html.replace("%name%", businessData.name);
   if (businessData.booze == "FALSE") {
     html = html.replace("businessInfo booze", "businessInfo booze nope");
@@ -28,20 +47,21 @@ const makeBusinessItem = (businessData) => {
   if (businessData.delivery == "FALSE") {
     html = html.replace("businessInfo delivery", "businessInfo delivery nope");
   }
-  html = html.replace("%deliveryPlace%", businessData.deliversTo);
-  if (businessData.deliversTo == "York") {
-    html = html.replace("class=\"business allBorder\"", "class=\"business yBorder\"")
-  } else if (businessData.deliversTo == "Harrogate & Knaresborough") {
-    html = html.replace("class=\"business allBorder\"", "class=\"business hkBorder\"")
-  };
+  // if (businessData.hk == "TRUE") {
+  //   html = html.replace("class=\"business yBorder\"", "class=\"business hkBorder\"")
+  // };
   html = html.replace("%notes%", businessData.notes);
   html = html.replace("%twitterLink%", businessData.twitter.substring(1));
   html = html.replace("%twitterName%", businessData.twitter);
   html = html.replace("%link%", businessData.link);
   html = html.replace("%name%", businessData.name);
-  document.querySelector(".businesses").insertAdjacentHTML("beforeend", html);
+  businesses.insertAdjacentHTML("beforeend", html);
 }
+
 
 //const locationButtons = Array.from(document.querySelectorAll("button"));
 //locationButtons.forEach(item => item.addEventListener("click", init));
-window.addEventListener('DOMContentLoaded', init)
+document.querySelector("#y").addEventListener('click', initYork)
+document.querySelector("#hk").addEventListener('click', initHk)
+
+// window.addEventListener('DOMContentLoaded', init)
